2fda3e6032543801e0c59b3344dd801d7e7766a1
Sharanya V <sharanya.v@s.amity.edu>
2025-03-04 11:43:45 -0800

    spacing

--- odd.c
+++ odd.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 int main() {
+
     int a;
     printf("Enter a number: ");
     scanf("%d", &a);
+
     if (a % 2 == 0) {
         printf("The number is even\n");
+
       } else {
         printf("The number is odd\n");
     }
