f50ec5778c5bd3bbae107f0c19f382c25ae2c8b1
Sharanya V <sharanya.v@s.amity.edu>
2025-03-04 11:06:30 -0800

    even

---
+++ even.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main() {
+    int a;
+    printf("Enter a number: ");
+    scanf("%d", &a);
+    if (a % 2 == 0) {
+        printf("The number is even\n");
+}    
+    return 0;
+}
+ 
