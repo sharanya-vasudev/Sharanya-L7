618972534469e885a0f1eec20116518feb48cd96
Sharanya V <sharanya.v@s.amity.edu>
2025-03-04 11:11:03 -0800

    odd

---
+++ odd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    printf("Enter a number: ");
+    scanf("%d", &a);
+    if (a % 2 == 0) {
+        printf("The number is even\n");
+      } else {
+        printf("The number is odd\n");
+    }
+    
+    return 0;
+}
+ 
